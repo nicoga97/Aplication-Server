a03a1c36bab7bdd08ea119eceb97c919cf9a5ca8
Nicolas <nicoga97@gmail.com>
2019-09-05 21:51:11 -0500

    Changes for implementation with Heroku

--- src/main/java/edu/escuelaing/arep/aplicationServer/WebAplicationService.java
+++ src/main/java/edu/escuelaing/arep/aplicationServer/WebAplicationService.java
@@ -4,16 +4,23 @@ import java.io.IOException;
 
 public class WebAplicationService {
 
-    public WebAplicationService() throws ReflectiveOperationException {
+    private WebAplicationService() throws ReflectiveOperationException {
         ListURLHandler handler = new ListURLHandler();
         handler.loadWebAplications();
 
-        initializeServer(35000, handler);
+        initializeServer(getPort(), handler);
 
 
     }
 
-    public void initializeServer(int port, ListURLHandler handler) throws ReflectiveOperationException {
+    private static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 4567;
+    }
+
+    private void initializeServer(int port, ListURLHandler handler) throws ReflectiveOperationException {
         try {
             new Server(port, handler);
         } catch (IOException e) {
